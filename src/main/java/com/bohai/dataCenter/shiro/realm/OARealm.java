35ec6c94493f9785f86d05555cf7de7236fdef5c
曹佳 <BHQH-CXYWB@BHQH-CXYWB-PC>
2017-09-14 13:10:10 +0800

    配置多realm策略  AtLeastOne

---
+++ src/main/java/com/bohai/dataCenter/shiro/realm/OARealm.java
@@ -0,0 +1,47 @@
+package com.bohai.dataCenter.shiro.realm;
+
+import org.apache.log4j.Logger;
+import org.apache.shiro.authc.AuthenticationException;
+import org.apache.shiro.authc.AuthenticationInfo;
+import org.apache.shiro.authc.AuthenticationToken;
+import org.apache.shiro.authc.SimpleAuthenticationInfo;
+import org.apache.shiro.authc.UnknownAccountException;
+import org.apache.shiro.realm.AuthenticatingRealm;
+import org.springframework.beans.factory.annotation.Autowired;
+
+import com.alibaba.fastjson.JSON;
+import com.bohai.dataCenter.entity.SysUser;
+import com.bohai.dataCenter.persistence.SysUserMapper;
+
+/**
+ * OA单点登录验证
+ * @author caojia
+ *
+ */
+public class OARealm extends AuthenticatingRealm {
+    
+    static Logger logger = Logger.getLogger(OARealm.class);
+    
+    @Autowired
+    private SysUserMapper sysUserMapper;
+
+
+    @Override
+    protected AuthenticationInfo doGetAuthenticationInfo(AuthenticationToken token) throws AuthenticationException {
+        
+        String username = (String) token.getPrincipal();
+        String password = new String((char[])token.getCredentials());
+        
+        SysUser sysUser = this.sysUserMapper.queryUserByUsername(username);
+        if(sysUser == null){
+            logger.warn("无此用户信息,用户名："+username);
+            throw new UnknownAccountException("无此用户信息,用户名："+username);
+        }
+        
+        logger.info("token: "+JSON.toJSONString(token));
+        
+        return new SimpleAuthenticationInfo(username, password, getName());
+        
+    }
+
+}
