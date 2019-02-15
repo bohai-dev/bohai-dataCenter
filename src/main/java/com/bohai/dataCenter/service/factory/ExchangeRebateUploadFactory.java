80ae9d8ef1508856a08c177241d00241996e26d2
曹佳 <BHQH-CXYWB@BHQH-CXYWB-PC>
2019-02-15 09:16:59 +0800

    交返增加能源中心

--- src/main/java/com/bohai/dataCenter/service/factory/ExchangeRebateUploadFactory.java
+++ src/main/java/com/bohai/dataCenter/service/factory/ExchangeRebateUploadFactory.java
@@ -18,7 +18,8 @@ public class ExchangeRebateUploadFactory implements UploadFactory {
 		}else if (fileName.indexOf("liquiddetails")>-1) {
 			//平仓明细数据上传
 			service = (FileUploadService) SpringContextUtil.getBean("closeDataTXTUploadService");
-		}else if (fileName.indexOf("大商所")>-1 || fileName.indexOf("上期所")>-1 || fileName.indexOf("郑商所")>-1 || fileName.indexOf("中金所")>-1) {
+		}else if (fileName.indexOf("大商所")>-1 || fileName.indexOf("上期所")>-1 || fileName.indexOf("郑商所")>-1 
+		        || fileName.indexOf("中金所")>-1 || fileName.indexOf("能源中心")>-1) {
 			//ctp交易明细数据上传
 			service = (FileUploadService) SpringContextUtil.getBean("ctpTradeDataCSVUpload");
 		}
